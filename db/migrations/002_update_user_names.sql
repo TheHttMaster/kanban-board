ee1c6880f14dce6e38306cff5c4537227f60ddc7
TheHttMaster <diperishilla2468@gmail.com>
2026-07-26 06:51:36 -0400

    f

--- db/migrations/002_update_user_names.sql
+++ db/migrations/002_update_user_names.sql
@@ -1,5 +1,8 @@
 -- Normaliza nombres de usuario antiguos a Kevin/Geral y actualiza las restricciones de la tabla.
 
+ALTER TABLE tasks DROP CONSTRAINT IF EXISTS tasks_assigned_to_check;
+ALTER TABLE tasks DROP CONSTRAINT IF EXISTS tasks_created_by_check;
+
 UPDATE tasks
 SET assigned_to = CASE assigned_to
   WHEN 'Ana' THEN 'Kevin'
@@ -16,10 +19,8 @@ SET created_by = CASE created_by
 END
 WHERE created_by IN ('Ana', 'Carlos');
 
-ALTER TABLE tasks DROP CONSTRAINT IF EXISTS tasks_assigned_to_check;
 ALTER TABLE tasks ADD CONSTRAINT tasks_assigned_to_check
   CHECK (assigned_to IN ('Kevin', 'Geral') OR assigned_to IS NULL);
 
-ALTER TABLE tasks DROP CONSTRAINT IF EXISTS tasks_created_by_check;
 ALTER TABLE tasks ADD CONSTRAINT tasks_created_by_check
   CHECK (created_by IN ('Kevin', 'Geral') OR created_by IS NULL);
